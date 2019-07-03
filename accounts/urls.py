2d8457a0cf89f11e334a9f093483f2b889f1f6a9
Gokula Krishnan Ravi <gkris4062@gmail.com>
2019-07-03 23:36:45 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,12 +6,12 @@ app_name = 'accounts'
 
 
 urlpatterns = [
-    url(r"^role1login/$", auth_views.LoginView.as_view(template_name="accounts/role1/login.html",redirect_authenticated_user=True,authentication_form=forms.RoleOneLogin),name='role1_login'),
+    url(r"^role1login/$", auth_views.LoginView.as_view(template_name="accounts/role1/login.html",
+        extra_context={'next': '#',},redirect_authenticated_user=True,authentication_form=forms.RoleOneLogin),
+        name='role1_login'),
     url(r"^role1logout/$", auth_views.LogoutView.as_view(), name="role1_logout"),
     url(r"^role1signup/$", views.RoleOneSignUp.as_view(), name="role1_signup"),
-    url(r"^role2login/$", auth_views.LoginView.as_view(template_name="accounts/role2/login.html",
-        extra_context={'next': '#',},redirect_authenticated_user=True,authentication_form=forms.RoleTwoLogin),
-        name='role2_login'),
+    url(r"^role2login/$", auth_views.LoginView.as_view(template_name="accounts/role2/login.html",redirect_authenticated_user=True,authentication_form=forms.RoleTwoLogin),name='role2_login'),
     url(r"^role2logout/$", auth_views.LogoutView.as_view(), name="role2_logout"),
     url(r"^role2signup/$", views.RoleTwoSignUp.as_view(), name="role2_signup"),
 ]
